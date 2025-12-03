1565b57cc053ebbc89f86b74483e326acbe829d2
ISHAN DAS <217930261+ishandas71006@users.noreply.github.com>
2025-12-03 20:39:34 +0530

    Calculating Simple Interest.

---
+++ Simple_Interest.py
@@ -0,0 +1,9 @@
+#Taking input from the user
+
+p=float(input("Enter your principal value ::"))
+t=float(input("Enter your time duration ::"))
+r=float(input("Enter your rate of interest ::"))
+
+#Calculating the Simple Interest
+
+print("Simple Interest =",(p*t*r)/100)
