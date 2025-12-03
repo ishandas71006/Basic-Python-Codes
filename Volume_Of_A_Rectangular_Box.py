683459fa9fbf882956dd30562aa855d7df9d2af5
ISHAN DAS <217930261+ishandas71006@users.noreply.github.com>
2025-12-03 20:39:11 +0530

    Calculating Volume Of a Rectangular Box.

---
+++ Volume_Of_A_Rectangular_Box.py
@@ -0,0 +1,7 @@
+#Taking input from the user
+l=float(input("Enter Length ::"))
+b=float(input("Enter Breadth ::"))
+h=float(input("Enter Height ::"))
+
+#Calculating the volume
+print("Volume of the rectangular box is :",l*b*h)
