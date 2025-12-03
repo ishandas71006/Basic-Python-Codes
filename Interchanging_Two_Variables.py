a3f594cb2bb2ededca31dda595f75a5945397733
ISHAN DAS <217930261+ishandas71006@users.noreply.github.com>
2025-12-03 20:40:51 +0530

    Interchanging Two Variable Without a 3rd one.

---
+++ Interchanging_Two_Variables.py
@@ -0,0 +1,8 @@
+#Taking input from the user
+var1=float(input("Enter 1st Variable ::"))
+var2=float(input("Enter 2nd Variable ::"))
+
+#interchanging the variables
+var1,var2 = var2,var1
+print("1st Variable after interchange is:",var1)
+print("2nd Variable after interchange is:",var2)
